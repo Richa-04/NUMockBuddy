c44c7b7f81f7c81da7b612afcbcf0bd88cfafa57
Aparajita Sharma <135308342+aparajitaSharma01@users.noreply.github.com>
2026-03-18 23:42:05 -0700

    Add homepage UI and basic structure

---
+++ app/practice/page.tsx
@@ -0,0 +1,21 @@
+import HeroSection from '@/components/home/HeroSection'
+import FeaturesSection from '@/components/home/FeaturesSection'
+import ExpertPanelSection from '@/components/home/ExpertPanelSection'
+import HowItWorksSection from '@/components/home/HowItWorksSection'
+import VolunteersSection from '@/components/home/VolunteersSection'
+import ResumeSection from '@/components/home/ResumeSection'
+import CTASection from '@/components/home/CTASection'
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <ExpertPanelSection />
+      <HowItWorksSection />
+      <VolunteersSection />
+      <ResumeSection />
+      <CTASection />
+    </>
+  )
+}
