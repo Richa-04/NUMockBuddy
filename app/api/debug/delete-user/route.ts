cb3dc7156333908334956f1cc80ffa6da505e613
Richa-p44 <padhariya.r@northeastern.edu>
2026-03-25 21:21:07 -0700

    delete debug file

--- app/api/debug/delete-user/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '@/lib/prisma'
-import { NextRequest } from 'next/server'
-
-export async function DELETE(req: NextRequest) {
-  const { nuid } = await req.json()
-  await prisma.user.delete({ where: { nuid } })
-  return Response.json({ deleted: true })
-}
